90549cc0261a1129639551b9a9a3dbd365ad2815
oussamaef06 <oussamaelfarihi@gmail.com>
2023-12-26 09:52:44 +0100

    modified login logic

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -1,34 +1,9 @@
 <?php
-
-global $db;
-
-if (isset($_POST['submit'])) {
-    $email = $db->real_escape_string($_POST['email']);
-    $password = $_POST['password'];
-
-    $stmt = $db->prepare("SELECT id_user, email, password FROM user WHERE email = ?");
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($result->num_rows > 0) {
-        $user = $result->fetch_assoc();
-
-        if (password_verify($password, $user['password'])) {
-            $_SESSION['user_id'] = $user['id'];
-            $_SESSION['user_name'] = $user['name'];
-            $_SESSION['user_email'] = $user['email'];
-
-            header("Location: index.php?page=home"); 
-            exit;
-        } else {
-            $error_message = "Invalid email or password.";
-        }
-    } else {
-        $error_message = "Invalid email or password.";
+    if(isset($_POST['submit'])){
+        session_unset();
+        session_destroy();
     }
-
-    $stmt->close();
-}
-
-?>
+    
+    if(isset($_POST['submit'])){
+        User::getUser($_POST["email"], $_POST["password"]);
+    } 
