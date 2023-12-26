1fcd00d622270d7fd9ed7f303d9f5f177a04c90f
oussamaef06 <oussamaelfarihi@gmail.com>
2023-12-26 09:53:06 +0100

    modified register logic

--- controllers/register_controller.php
+++ controllers/register_controller.php
@@ -1,24 +1,7 @@
 <?php
-
-global $db;
-if (isset($_POST['submit'])) {
-    $name = mysqli_real_escape_string($db, $_POST['name']);
-    $email = mysqli_real_escape_string($db, $_POST['email']);
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-
-    
-    $stmt = $db->prepare("INSERT INTO user (name, email, password) VALUES (?, ?, ?)");
-    $stmt->bind_param("sss", $name, $email, $password);
-    $result = $stmt->execute();
-
-    if ($result) {
-        header('Location: ../views/login_view.php'); 
-        exit();
-    } else {
-        $error_message = "Registration failed: " . $db->error;
-        include('../views/register_view.php'); 
-    }
+if(isset($_POST['submit'])){
+    $newUser = new User($_POST['name'], $_POST["email"], $_POST["password"]);
+    $newUser->insertUser();
 }
-
 
 ?>
